ec1e766a8a01b0392db2f2229f9dd86c9c190a37
AlexKiprono <lexiekiprono@gmail.com>
2024-04-24 11:49:12 +0300

    jsx lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/data";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+
+  const divStyle = {
+    color: 'firebrick',
+  };
+
+  return <div id="home">Home
+            <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+         </div>;
 }
 
 export default Home;
